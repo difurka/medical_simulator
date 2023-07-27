223ca8346f7cbabf240d5b42c160c3e858122255
cshara <cshara@student.21-school.ru>
2023-07-27 12:12:53 +0700

    add button

--- src/medical_simulator/mainwindow.cpp
+++ src/medical_simulator/mainwindow.cpp
@@ -13,3 +13,13 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    if (ui->label->text() == "ON") {
+        ui->label->setText("OFF");
+    } else {
+        ui->label->setText("ON");
+    }
+}
+
